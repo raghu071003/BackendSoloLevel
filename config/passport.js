59434c3663ae7a82fb7296bec8c2a65c58528020
Sai Raghu Vamshi Koneti <131893227+raghu071003@users.noreply.github.com>
2025-08-14 19:13:04 +0530

    Bug

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ import { Progress } from "../models/progress.model.js";
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `https://backendsololevel.onrender.com/api/v1/user/auth/google/callback`
+    callbackURL: "https://backendsololevel.onrender.com/api/v1/user/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
